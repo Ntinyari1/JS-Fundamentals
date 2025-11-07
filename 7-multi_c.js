c177ff566b932e83196b7588841be8925afca2eb
Ntinyari1 <ntinyariyvonne823@gmail.com>
2025-11-07 20:06:05 +0300

    square

---
+++ 7-multi_c.js
@@ -0,0 +1,14 @@
+const arg = process.argv[2];
+const x = parseInt(arg);
+
+if (isNaN(x) || arg === undefined) {
+ console.log("Missing number of occurrences");
+} else {
+ 
+  let i = 0;
+  while (i < x) {
+   
+    console.log("C is fun");
+    i++;
+  }
+}
